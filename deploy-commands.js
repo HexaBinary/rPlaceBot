8a048fe8f80ba4ac02904f98cad8a66691a13e45
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2022-04-03 12:47:52 +0300

    *

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId } = require('./config.json');
+const { clientId, guildId } = require('./config.json');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -15,17 +15,6 @@ const commands = [
 
 	const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
-	(async () => {
-		try {
-			console.log('Started refreshing application (/) commands.');
-	
-			await rest.put(
-				Routes.applicationCommands(clientId),
-				{ body: commands },
-			);
-	
-			console.log('Successfully reloaded application (/) commands.');
-		} catch (error) {
-			console.error(error);
-		}
-	})();
+	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
